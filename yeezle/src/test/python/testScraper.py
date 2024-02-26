939102ae325e03d821dddbd64099d82df92110fa
Luke <lce1868@g.rit.edu>
2024-02-26 16:48:22 -0500

    fixing ci

--- yeezle/src/test/python/testScraper.py
+++ yeezle/src/test/python/testScraper.py
@@ -1,11 +1,6 @@
 import unittest
-from python import scraper
 
 class testScraper(unittest.TestCase):
 
     def test_module_load(self):
         self.assertEqual('test'.upper(), 'TEST')
-
-    
-if __name__ == "__main__":
-    unittest.main()
